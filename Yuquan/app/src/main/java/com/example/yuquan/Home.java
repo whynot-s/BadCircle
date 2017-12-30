c56460a7f47bea6fc7bfa2a64d308872c71c94c3
zjf <1061683512@qq.com>
2017-12-31 00:13:52 +0800

    2017/12/31/0/12

--- Yuquan/app/src/main/java/com/example/yuquan/Home.java
+++ Yuquan/app/src/main/java/com/example/yuquan/Home.java
@@ -1,10 +1,15 @@
 package com.example.yuquan;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 /**
  * Created by terry on 2017/11/28.
  */
 
-public class Home extends Activity{
+public class Home extends Activity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home);
+    }
 }
